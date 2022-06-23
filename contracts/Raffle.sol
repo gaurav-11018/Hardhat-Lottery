4592f327ffe457c70e9d640138d570ea8788dcbb
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-06-23 23:02:13 +0530

    Add files via upload

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,23 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
-//error Raffle__NotEnoughETHEntered();
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract Raffle {
+error Raffle__SendMoreToEnterRaffle();
+
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private i_entranceFee;
+    address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    event RaffleEnter(address indexed s_player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {
+    function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
-            //           revert Raffle__NotEnoughETHEntered();
+            revert Raffle__SendMoreToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    //function pickRandomWinner(){}
+    function requestRandomWinner() external {}
+
+    function fulfilRandomWord(uint256 reqestId, uint256[] memory randomWords) internal {}
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
